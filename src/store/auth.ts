8123cc6c121280f0047ea2553d2a6309fa83f545
PMO-APP <fittbrandng@gmail.com>
2026-04-29 20:31:50 +0100

    Update auth.ts

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -7,16 +7,28 @@ interface AuthState {
   loading: boolean
   setUser: (user: Profile | null) => void
   setLoading: (v: boolean) => void
+  initialize: () => Promise<void>
   signOut: () => Promise<void>
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
   user: null,
   loading: true,
+
   setUser: (user) => set({ user }),
   setLoading: (loading) => set({ loading }),
+
+  initialize: async () => {
+    const { data } = await supabase.auth.getSession()
+
+    set({
+      user: data.session?.user as any || null,
+      loading: false
+    })
+  },
+
   signOut: async () => {
     await supabase.auth.signOut()
-    set({ user: null })
+    set({ user: null, loading: false })
   },
 }))
