6c83d0b35c232f28978fdfe60413448f7240259c
PMO-APP <fittbrandng@gmail.com>
2026-04-29 20:35:19 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import './index.css'
+import { useAuthStore } from './store/auth'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,7 +14,7 @@ const queryClient = new QueryClient({
     },
   },
 })
-
+useAuthStore.getState().initialize()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
