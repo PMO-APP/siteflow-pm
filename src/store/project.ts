35545b0e180bb926f3496bd0cc25dc2487562aa5
PMO-APP <fittbrandng@gmail.com>
2026-05-04 10:49:13 +0100

    Update project.ts

--- src/store/project.ts
+++ src/store/project.ts
@@ -3,42 +3,34 @@ import { create } from 'zustand'
 interface ProjectState {
   projectId: number | null
   projectName: string
-  setProject: (
-    id: number,
-    name: string
-  ) => void
+  setProject: (id: number, name: string) => void
+  clearProject: () => void
 }
 
-export const useProjectStore =
-create<ProjectState>(set => ({
-  projectId: Number(
-    localStorage.getItem(
-      'projectId'
-    )
-  ) || 1,
+export const useProjectStore = create<ProjectState>(set => ({
+  projectId: localStorage.getItem('projectId')
+    ? Number(localStorage.getItem('projectId'))
+    : null,
 
-  projectName:
-    localStorage.getItem(
-      'projectName'
-    ) || 'Lakowe Spa',
+  projectName: localStorage.getItem('projectName') || '',
 
-  setProject: (
-    id,
-    name
-  ) => {
-    localStorage.setItem(
-      'projectId',
-      String(id)
-    )
-
-    localStorage.setItem(
-      'projectName',
-      name
-    )
+  setProject: (id, name) => {
+    localStorage.setItem('projectId', String(id))
+    localStorage.setItem('projectName', name)
 
     set({
       projectId: id,
       projectName: name
+    })
+  },
+
+  clearProject: () => {
+    localStorage.removeItem('projectId')
+    localStorage.removeItem('projectName')
+
+    set({
+      projectId: null,
+      projectName: ''
     })
   }
 }))
