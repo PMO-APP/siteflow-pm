7a4035c134324bed97b48e93a4e5658dcea5cc0e
PMO-APP <fittbrandng@gmail.com>
2026-06-15 09:23:12 +0100

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -12,7 +12,7 @@ import { useEffect, useState } from 'react'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 import {
   LayoutDashboard,
-  BriefcaseBusiness,
+  Briefcase,
   CalendarDays,
   ShoppingCart,
   CheckSquare,
