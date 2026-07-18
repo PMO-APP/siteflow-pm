9d54af871abea9c7e6a75a77375d30736aaac172
PMO-APP <fittbrandng@gmail.com>
2026-07-18 13:55:57 +0100

    Add files via upload

---
+++ src/components/external/ExternalNavigation.tsx
@@ -0,0 +1,9 @@
+export const consultantNav=[
+'Projects','Drawings','Submissions','Approvals','Meetings','Documents'
+];
+export const contractorNav=[
+'Projects','Tasks','Daily Reports','Inspections','Snags','RFIs','Documents'
+];
+export const vendorNav=[
+'Purchase Orders','Deliveries','Invoices','Documents'
+];
