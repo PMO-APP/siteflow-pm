3dc2a429861c050013a4fb4bff7149df8fdbe258
PMO-APP <fittbrandng@gmail.com>
2026-07-18 14:03:31 +0100

    Add files via upload

---
+++ src/components/consultant/ConsultantMenu.ts
@@ -0,0 +1,11 @@
+export const consultantMenu=[
+{title:'Dashboard',path:'/external'},
+{title:'Projects',path:'/external/projects'},
+{title:'Drawings',path:'/external/drawings'},
+{title:'Submissions',path:'/external/submissions'},
+{title:'Reviews',path:'/external/reviews'},
+{title:'Approvals',path:'/external/approvals'},
+{title:'Meetings',path:'/external/meetings'},
+{title:'Technical Queries',path:'/external/queries'},
+{title:'Documents',path:'/external/documents'}
+];
