6cb0409ff8252ae13111ad706284a095399dfd98
PMO-APP <fittbrandng@gmail.com>
2026-07-18 13:58:47 +0100

    Add files via upload

---
+++ src/components/contractor/ContractorMenu.ts
@@ -0,0 +1,11 @@
+export const contractorMenu=[
+{title:'Dashboard',path:'/external'},
+{title:'Tasks',path:'/external/tasks'},
+{title:'Daily Reports',path:'/external/daily-reports'},
+{title:'RFIs',path:'/external/rfis'},
+{title:'Inspections',path:'/external/inspections'},
+{title:'Snags',path:'/external/snags'},
+{title:'Progress Photos',path:'/external/photos'},
+{title:'Materials',path:'/external/materials'},
+{title:'Documents',path:'/external/documents'}
+];
