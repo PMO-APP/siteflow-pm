7f34284d00a28d59310f465c46985bb7648570d6
PMO-APP <fittbrandng@gmail.com>
2026-07-30 16:04:19 +0100

    Update index.ts

--- src/components/health/index.ts
+++ src/components/health/index.ts
@@ -4,3 +4,9 @@ export * from './ProjectHealthCard'
 export * from './HealthContributors'
 export * from './HealthBreakdownChart'
 export * from './HealthDetailsDrawer'
+
+export { HealthHistoryChart } from './HealthHistoryChart'
+
+export { ExecutiveHealthReportPanel } from './ExecutiveHealthReportPanel'
+
+export { PortfolioHealthComparison } from './PortfolioHealthComparison'
