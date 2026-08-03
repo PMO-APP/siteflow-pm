5b2e3bc27d90b0b9a76392a04b3b81fb2feb838d
PMO-APP <fittbrandng@gmail.com>
2026-08-03 10:53:19 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -66,6 +66,7 @@ const ExecutiveDashboardPage = lazy(() => import('@/pages/ExecutiveDashboardPage
 const ExecutiveNarrativePage = lazy(() => import('@/pages/ExecutiveNarrativePage'))
 const ReportDesignerPage = lazy(() => import('@/pages/ReportDesignerPage'))
 const ReportDistributionPage = lazy(() => import('@/pages/ReportDistributionPage'))
+const BoardroomPage = lazy(() => import('@/pages/BoardroomPage'))
 const CustomerAdministrationPage = lazy(() => import('@/pages/CustomerAdministrationPage'))
 
 
@@ -475,6 +476,8 @@ export default function App() {
           <Route path="executive-narrative" element={<ExecutiveNarrativePage />} />
           <Route path="report-designer" element={<ReportDesignerPage />} />
           <Route path="report-distribution" element={<ReportDistributionPage />} />
+          <Route path="boardroom" element={<BoardroomPage />} />
+
 
 
 
